905c1c58b7853dafde225d9729ec8f9eccd03944
RushikeshKokate22 <rushikesh.kokate@tradebrains.in>
2024-08-12 15:57:42 +0530

    Enhanced navbar design with padding, shadows, and modern styling improvements. Searchbar implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Dashboard from "./ui/Dashboard";
+import Navbar from "./ui/Navbar";
+
  
 
 function App() {
   return (
     <div className="App">
-     
+        <Navbar/>
+     <Dashboard/>
     </div>
   );
 }
---
+++ src/ui/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        Dashboard
+        <input type="text"  className='searchbar' placeholder='Search anything...'/>
+        
+    </div>
+  )
+}
+
+export default Navbar
